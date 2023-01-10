f5ab14fa7e6889f64e54dff7d4e10f495033c539
Mihai Galos <mihaigalos@gmail.com>
2023-01-10 19:17:03 +0100

    Dot: Generate edges

--- Justfile
+++ Justfile
@@ -1,10 +1,12 @@
 @_default:
     just --list --unsorted
 
-preview:
+preview: run
     #!/bin/bash
-    xdot <(dot test/expected.dot)
+    xdot <(dot /tmp/actual.dot)
 
-@test:
+run:
     go run src/main.go > /tmp/actual.dot
+
+@test: run
     [ $(diff test/expected.dot /tmp/actual.dot | wc -l) -eq 0 ] && echo '\e[1;32mOK\e[0m' || echo "\e[1;31mERROR: test/expected.dot incompatible with /tmp/actual.dot\e[0m"
--- src/main.go
+++ src/main.go
@@ -3,15 +3,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"text/template"
 )
 
-type PullRequestData struct {
+type PullRequest struct {
 	Name  string
 	Href  string
 	State string
 	Title string
+
+	Dependencies []*PullRequest
 }
 
 var header = `digraph D {
@@ -60,11 +63,11 @@ func main() {
 	t.New("pr_open").Parse(pr_open)
 	t.New("pr_merged").Parse(pr_merged)
 	t.New("pr_closed").Parse(pr_closed)
-	prs := []PullRequestData{
-		{"aim/208", "https://github.com/mihaigalos/aim/pull/208", "open", "Fix vulnerability CVE-2022-23639"},
-		{"aim/189", "https://github.com/mihaigalos/aim/pull/189", "merged", "Http serve: Propagate result"},
-		{"aim/182", "https://github.com/mihaigalos/aim/pull/182", "closed", "Fix OpenSSL dep - revision 300.0.10+3.0.6 yanked"},
-	}
+	pr_aim_182 := PullRequest{"aim/182", "https://github.com/mihaigalos/aim/pull/182", "closed", "Fix OpenSSL dep - revision 300.0.10+3.0.6 yanked", nil}
+	pr_aim_189 := PullRequest{"aim/189", "https://github.com/mihaigalos/aim/pull/189", "merged", "Http serve: Propagate result", []*PullRequest{&pr_aim_182}}
+	pr_aim_208 := PullRequest{"aim/208", "https://github.com/mihaigalos/aim/pull/208", "open", "Fix vulnerability CVE-2022-23639", []*PullRequest{&pr_aim_189}}
+
+	prs := []PullRequest{pr_aim_208, pr_aim_189, pr_aim_182}
 	t.ExecuteTemplate(os.Stdout, "header", "")
 	for _, pr := range prs {
 		switch pr.State {
@@ -74,6 +77,13 @@ func main() {
 			t.ExecuteTemplate(os.Stdout, "pr_merged", pr)
 		case "closed":
 			t.ExecuteTemplate(os.Stdout, "pr_closed", pr)
+		}
+	}
+	for _, pr := range prs {
+		if pr.Dependencies != nil {
+			for _, dep := range pr.Dependencies {
+				fmt.Println("    \"" + pr.Name + "\" -> \"" + dep.Name + "\"")
+			}
 		}
 	}
 	t.ExecuteTemplate(os.Stdout, "footer", "")
