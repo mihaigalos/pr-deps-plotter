124831a7c108128bd57970476bf3d97d17dd76ef
Mihai Galos <mihaigalos@gmail.com>
2023-01-11 19:33:57 +0100

    Reader: Split

--- src/main.go
+++ src/main.go
@@ -4,10 +4,6 @@ package main
 
 
 func main() {
-	pr_aim_182 := PullRequest{"aim/182", "https://github.com/mihaigalos/aim/pull/182", "closed", "Fix OpenSSL dep - revision 300.0.10+3.0.6 yanked", nil}
-	pr_aim_189 := PullRequest{"aim/189", "https://github.com/mihaigalos/aim/pull/189", "merged", "Http serve: Propagate result", []*PullRequest{&pr_aim_182}}
-	pr_aim_208 := PullRequest{"aim/208", "https://github.com/mihaigalos/aim/pull/208", "open", "Fix vulnerability CVE-2022-23639", []*PullRequest{&pr_aim_189}}
-
-	prs := []PullRequest{pr_aim_208, pr_aim_189, pr_aim_182}
+	prs := read()
 	write(prs)
 }
