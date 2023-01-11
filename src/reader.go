2cffe0ad57cd16f81af0bb97bdeaef14a0e2d486
Mihai Galos <mihaigalos@gmail.com>
2023-01-11 21:03:56 +0100

    Reader,Writer: Move templates to writer

--- src/reader.go
+++ src/reader.go
@@ -1,44 +1,5 @@
 package main
 
-var header = `digraph D {
-
-    node [shape=plaintext]
-`
-
-var pr_open = `
-    "{{ .Name }}" [
-    label=<
-    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
-      <tr><td bgcolor="#008000"><font color="#ffffff">{{ .Name }}</font></td></tr>
-      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
-      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
-    </table>>
-  ];
-`
-var pr_merged = `
-    "{{ .Name }}" [
-    label=<
-    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
-      <tr><td bgcolor="#4c278d"><font color="#ffffff">{{ .Name }}</font></td></tr>
-      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
-      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
-    </table>>
-  ];
-`
-var pr_closed = `
-    "{{ .Name }}" [
-    label=<
-    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
-      <tr><td bgcolor="#a02f2b"><font color="#ffffff">{{ .Name }}</font></td></tr>
-      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
-      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
-    </table>>
-  ];
-`
-
-var footer = `
-}
-`
 func read() []PullRequest {
 	pr_aim_182 := PullRequest{"aim/182", "https://github.com/mihaigalos/aim/pull/182", "closed", "Fix OpenSSL dep - revision 300.0.10+3.0.6 yanked", nil}
 	pr_aim_189 := PullRequest{"aim/189", "https://github.com/mihaigalos/aim/pull/189", "merged", "Http serve: Propagate result", []*PullRequest{&pr_aim_182}}
--- src/writer.go
+++ src/writer.go
@@ -6,6 +6,44 @@ import (
 	"text/template"
 )
 
+var header = `digraph D {
+
+    node [shape=plaintext]
+`
+
+var pr_open = `
+    "{{ .Name }}" [
+    label=<
+    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
+      <tr><td bgcolor="#008000"><font color="#ffffff">{{ .Name }}</font></td></tr>
+      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
+      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
+    </table>>
+    ];
+`
+var pr_merged = `
+    "{{ .Name }}" [
+    label=<
+    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
+      <tr><td bgcolor="#4c278d"><font color="#ffffff">{{ .Name }}</font></td></tr>
+      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
+      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
+    </table>>
+    ];
+`
+var pr_closed = `
+    "{{ .Name }}" [
+    label=<
+    <table border="0" cellborder="1" cellspacing="0" href="{{ .Href }}">
+      <tr><td bgcolor="#a02f2b"><font color="#ffffff">{{ .Name }}</font></td></tr>
+      <tr><td bgcolor="#ffffff"><font color="#000000">{{ .State }}</font></td></tr>
+      <tr><td bgcolor="#657a7f"><font color="#ffffff">{{ .Title }}</font></td></tr>
+    </table>>
+    ];
+`
+
+var footer = `}
+`
 func write(prs []PullRequest){
 
 	t, _ := template.New("header").Parse(header)
@@ -25,6 +63,7 @@ func write(prs []PullRequest){
 			t.ExecuteTemplate(os.Stdout, "pr_closed", pr)
 		}
 	}
+	fmt.Println("")
 	for _, pr := range prs {
 		if pr.Dependencies != nil {
 			for _, dep := range pr.Dependencies {
